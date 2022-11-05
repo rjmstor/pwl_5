d8161fa7b78b3df4264121d202215f0aaed1319a
rian maulana <rianjijim123@gmail.com>
2022-11-05 09:55:20 +0700

    draf : otentikasi

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function index()
+    {
+        $user = Auth::user();
+        return view('home', compact('user'));
+    }
+}
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,22 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+    public function login(Request $request)
+    {
+        $input = $request->all();
+
+        $this->validate($request, [
+            'email'=>'required|email',
+            'password' => 'required'
+        ])
+        if(auth()->attemp(array('email' =>$input['email'], 'password' =>$input['password'])))
+        {
+            if (auth()->user()->roles_id == 1){
+                return redirect()->route('admin.home');
+            }else{
+                return redirect()->route('login')
+                ->with('email','Email-Address And Password Are Wrong')
+            }
+        }
+    }
 }
